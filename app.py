e1cfe549e94d9e06600daf48dd68e552830ce636
malak <malakmohamedsaad148@gmail.com>
2024-08-31 18:59:45 +0300

    app edit to calc

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
-print("hello my name is malak")
+
+first=float(input("enter first num= "))
+op=input("enter operation= ")
+second=float(input("enter second num= "))
+if op == "+":
+    print(first + second ) 
+elif op == "-":  
+    print(first - second ) 
+elif op == "/":  
+    print(first / second ) 
+elif op == "*":  
+    print(first * second )
+while (input(" For another operation enter y :") == 'y'):
+    first=float(input("enter first num= "))
+op=input("enter operation= ")
+second=float(input("enter second num= "))
+if op == "+":
+    print(first + second ) 
+elif op == "-":  
+    print(first - second ) 
+elif op == "/":  
+    print(first / second ) 
+elif op == "*":  
+    print(first * second )
